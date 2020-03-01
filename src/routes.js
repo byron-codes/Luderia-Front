929b0e3f96fa5483e27dee22890a51a74e5d0daf
Amanda Zingareli <amanda.zingareli@fatec.sp.gov.br>
2020-03-01 12:05:25 -0300

    Correcao do carrosel

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,7 @@ export default props => (
       <Route path="/itens" component={ItensList} exact></Route>
       <Route path="/itens/*" component={Item} exact></Route>
       <Route path="/cart" component={Cart} exact></Route>
-      <Route path="/cart/buy" component={Cart} exact></Route>
+      <Route path="/cart/buy" component={Buy} exact></Route>
       <Route path="/admin" component={Admin} exact></Route>
       <Redirect from="*" to="/"></Redirect>
     </Switch>
