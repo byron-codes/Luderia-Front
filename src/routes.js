16df848fb71afb90de8d5f5ff6ba98ea8347da11
Vinicius Moreira <vinicius.moraes@iteris.com.br>
2020-03-01 12:07:31 -0300

    wfknk

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,8 @@ import ItensList from "./screens/ItensList";
 import Item from "./screens/Item";
 import Cart from "./screens/Cart";
 import Buy from "./screens/Buy";
+import Login from "./screens/Login";
+
 
 export default props => (
   <Router>
@@ -22,6 +24,7 @@ export default props => (
       <Route path="/cart" component={Cart} exact></Route>
       <Route path="/cart/buy" component={Cart} exact></Route>
       <Route path="/admin" component={Admin} exact></Route>
+      <Route path="/login" component={Login} exact></Route>
       <Redirect from="*" to="/"></Redirect>
     </Switch>
   </Router>
